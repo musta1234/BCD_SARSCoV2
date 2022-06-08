6cae038696c5e69a1b8f2642c2ad6f2ff78b7bde
root <root@DESKTOP-M1SK02D.localdomain>
2022-06-08 11:49:50 -0400

    select the most recent nexus and nwk files

--- code/convert_newick_to_csv.R
+++ code/convert_newick_to_csv.R
@@ -5,8 +5,30 @@ rm( list = ls())
 
 # load libraries
 preq = c( "tidyverse", "ggplot2", "readxl", "readr", 
-          "stats", "ade4", "ape", "adegenet", "gitcreds" )
+          "stats", "ade4", "ape", "adegenet" )
 #for (y in preq) install.packages(y, dep = TRUE)
 sapply(preq, library, character.only=T)
+rm( list = ls())
 
-inloc <- "./"
+inloc <- "./data/"
+#import nexus and nwk files
+df <- file.info(list.files(inloc, full.names = T))
+df$filename <- row.names(df)
+df_nwk <- df %>% 
+  filter( . , grepl("nwk$", filename, perl = T )) %>% 
+  filter(mtime == max(mtime)) %>% select(filename) %>% 
+  unlist()
+df_nex <- df %>% 
+  filter( . , grepl("nexus$", filename, perl = T )) %>% 
+  filter(mtime == max(mtime)) %>% select(filename) %>%
+  unlist()
+
+df_nex <- df %>% 
+  filter( . , grepl("nwk", filename ))
+
+
+latest_newick <- 
+  
+rownames(df)[which.max(df$mtime)]
+
+#[1] "/path/to/your/directory/Interested_file.xlsx"
